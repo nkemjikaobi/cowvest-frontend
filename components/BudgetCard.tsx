bfdcfbca2526b8cf43743c86bbe6a3d006ca17a4
Obi Nkemjika Derick <obinkemjikaderick@Administrators-MacBook-Pro.local>
2022-04-28 12:23:41 +0100

    Disable when expired

--- components/BudgetCard.tsx
+++ components/BudgetCard.tsx
@@ -9,6 +9,7 @@ import 'moment-timezone';
 import AuthContext from 'context/auth/AuthContext';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import moment from 'moment-timezone';
 
 const BudgetCard = ({ handleExpenseChange, budget }: any) => {
 	const router = useRouter();
@@ -17,6 +18,7 @@ const BudgetCard = ({ handleExpenseChange, budget }: any) => {
 	const { getExpenses } = authContext;
 
 	const [progress, setProgress] = useState<number>(0);
+	const [isExpired, setIsExpired] = useState<boolean>(false);
 
 	const handleViewExpenses = () => {
 		getExpenses(budget._id);
@@ -35,9 +37,12 @@ const BudgetCard = ({ handleExpenseChange, budget }: any) => {
 		if (budget) {
 			const result = calculateProgress(budget.start_price, budget.max_spending);
 			setProgress(result);
+			const expiry = moment(new Date()).isAfter(budget.end_date);
+			setIsExpired(expiry);
 		}
 		//eslint-disable-next-line
 	}, [budget]);
+
 	return (
 		<div className='bg-black drop-shadow-md rounded-lg p-5 mb-4 md:mb-0'>
 			<div className='flex justify-between items-center'>
@@ -62,13 +67,22 @@ const BudgetCard = ({ handleExpenseChange, budget }: any) => {
 				/>
 			</div>
 			<div>
-				<div className='mb-4 flex justify-between items-center'>
+				<div className='mb-4 flex justify-between items-center text-white'>
 					<p>
 						<Moment format='MMM Do YYYY' date={budget.start_date} /> - {''}
 						<Moment format='MMM Do YYYY' date={budget.end_date} />
 					</p>
-					{budget.hasExpired ? (
+					{/* {budget.hasExpired ? (
 						<button className='border bg-red-500 border-black px-5 py-3 rounded-lg hover:bg-green-600'>
+							Expired
+						</button>
+					) : (
+						<button className='border bg-green-500 border-black px-5 py-3 rounded-lg hover:bg-green-600'>
+							Active
+						</button>
+					)} */}
+					{isExpired ? (
+						<button className='border bg-red-500 border-black px-5 py-3 rounded-lg hover:bg-red-600'>
 							Expired
 						</button>
 					) : (
@@ -80,13 +94,17 @@ const BudgetCard = ({ handleExpenseChange, budget }: any) => {
 				<hr className='mb-4' />
 				<div className='flex justify-between flex-col md:flex-row items-center'>
 					<button
-						className='border bg-purple-900 border-black px-5 py-3 rounded-lg hover:bg-blue-900 w-full md:w-2/5 mb-4 md:mb-0'
+						className={`border bg-purple-900 border-black px-5 py-3 rounded-lg hover:bg-blue-900 w-full md:w-2/5 mb-4 md:mb-0 ${
+							isExpired && 'pointer-events-none bg-purple-400 opacity-40'
+						}`}
 						onClick={() => handleExpenseChange(true, budget._id)}
 					>
 						Add Expense
 					</button>
 					<button
-						className='border bg-purple-900 border-black px-5 py-3 rounded-lg hover:bg-blue-900 w-full md:w-2/5'
+						className={`border bg-purple-900 border-black px-5 py-3 rounded-lg hover:bg-blue-900 w-full md:w-2/5 ${
+							isExpired && 'pointer-events-none bg-purple-400 opacity-40'
+						}`}
 						onClick={() => handleViewExpenses()}
 					>
 						View Expenses
